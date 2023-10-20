f4029ed86da16b74a8f34de32650b361c2d99e0b
Diego Acosta <djacosta.07@gmail.com>
2023-10-19 20:25:19 -0700

    adding dataset to repo

--- hw2_kaggle_dataset.py
+++ hw2_kaggle_dataset.py
@@ -1,14 +1,14 @@
 import pandas as pd
 import sqlite3
 
-df = pd.read_csv("# DATA FILE")
+df = pd.read_csv("fifa_dataset/male_teams.csv")
 print(df.head())
 print(df.tail())
 
 
-connection = sqlite3.connect("# DATABASE FILENAME")
-cursor = connection.cursor()
-cursor.execute("DROP TABLE IF EXISTS #[table name]")
+# connection = sqlite3.connect("# DATABASE FILENAME")
+# cursor = connection.cursor()
+# cursor.execute("DROP TABLE IF EXISTS #[table name]")
 
-df.to_sql("#[table name]", connection)
-connection.close()
+# df.to_sql("#[table name]", connection)
+# connection.close()
