366e773c565bceca9aacde49f5a3a223cf6c5b7e
Diego Acosta <djacosta.07@gmail.com>
2023-10-20 17:25:10 -0700

    finished assignment

--- hw2_kaggle_dataset.py
+++ hw2_kaggle_dataset.py
@@ -1,14 +1,38 @@
 import pandas as pd
-import sqlite3
+import json
+from sqlalchemy import create_engine, Column, String, MetaData, Table
 
 df = pd.read_csv("fifa_dataset/male_teams.csv")
-print(df.head())
-print(df.tail())
+print(f"head: {df.head()}")
+print(f"tail: {df.tail()}")
 
+# Getting database credentials 
+with open('db_config.json') as db_config:
+    config = json.load(db_config)
 
-# connection = sqlite3.connect("# DATABASE FILENAME")
-# cursor = connection.cursor()
-# cursor.execute("DROP TABLE IF EXISTS #[table name]")
+db_host = config["host"]
+db_user = config["user"]
+db_password = config["password"]
+db_name = config["database"]
 
-# df.to_sql("#[table name]", connection)
-# connection.close()
+# Connecting to MySQL database
+db_url = f'mysql+mysqlconnector://{db_user}:{db_password}@{db_host}/{db_name}'
+engine = create_engine(db_url)
+
+# Creating table
+table_name = 'fifa_stats'
+
+metadata = MetaData()
+
+columns = [Column(col, String(255)) for col in df.columns]
+
+new_table = Table(table_name, metadata, *columns)
+
+metadata.create_all(engine)
+
+# Uploading dataframe to MySQL
+df.to_sql(table_name, engine, if_exists='append', index=False)
+
+# Closing the connection
+engine.dispose()
+
